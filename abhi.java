5cb9737a1e9ac4e3a0bdbfdd663ccc515af2fadd
AbhishekKumar0729 <buntykumar7742@gmail.com>
2024-04-30 12:14:32 +0530

    abhi

---
+++ abhi.java
@@ -0,0 +1,5 @@
+public class abhi {
+    public static void main(String[] args) {
+        System.out.println("laptop me ubuntu ko install nahi kar sakte hai , sirf virtual box se run kar sakte ahi");
+    }
+}
